9e2d14fa0a2f0d5636cef49dabf57786341b2bce
mickael-palma-wttj <mickael.palma-ext@wttj.co>
2025-12-07 19:16:42 +0000

    refactor: improve logger configuration by ensuring log directory exists before file creation

--- spec/support/logger_config.rb
+++ spec/support/logger_config.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require 'fileutils'
+
 # Redirect logger output to tmp file during tests
-test_log_file = File.open(File.expand_path('../../tmp/test.log', __dir__), 'w')
+log_dir = File.expand_path('../../tmp', __dir__)
+FileUtils.mkdir_p(log_dir)
+test_log_file = File.open(File.join(log_dir, 'test.log'), 'w')
 stdout_original = $stdout
 $stdout = test_log_file
 
